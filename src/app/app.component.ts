213225aa3911b0f5aaae1823af1114433137f535
System Administrator <root@MacBook-Pro-de-VIGUIER.local>
2017-12-04 11:05:44 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
+import { SplashScreen} from "@ionic-native/splash-screen";
+import { StatusBar} from "@ionic-native/status-bar";
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
-  templateUrl: 'app.html'
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) nav: Nav;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { SplashScreen} from "@ionic-native/splash-screen";
+import { StatusBar} from "@ionic-native/status-bar";
+import { Geolocation } from '@ionic-native/geolocation';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
@@ -22,9 +23,9 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
-    StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    StatusBar,
+    Geolocation
   ]
 })
 export class AppModule {}
